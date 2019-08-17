1056f325defe648de4ef8aa9268357bf5b6e2f53
lchaconc <luis.chacon.campos@gmail.com>
2019-08-16 22:59:39 -0600

    correción de granja
    animacion  en venta de productos

--- SQL/u211168474_confe.sql
+++ SQL/u211168474_confe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 14-08-2019 a las 04:55:15
+-- Tiempo de generación: 17-08-2019 a las 04:53:36
 -- Versión del servidor: 5.7.21
 -- Versión de PHP: 7.1.16
 
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `avatar` (
 --
 
 INSERT INTO `avatar` (`id_avatar`, `tipo_avatar`, `tipo_traje`, `sexo`, `id_usr`) VALUES
-(1, 5, 'pijama', 'm', 'felipe@correo.de'),
+(1, 2, 'hombre1', 'm', 'felipe@correo.de'),
 (2, 1, 'scout', 'f', 'jimena@correo.de'),
 (3, 0, '', 'f', 'ana@correo.de'),
 (4, 0, '', 'm', 'oscar.wong@correo.de');
@@ -66,7 +66,7 @@ CREATE TABLE IF NOT EXISTS `estado_juegos` (
   `avance` int(11) NOT NULL,
   `terminado` varchar(8) NOT NULL COMMENT 'boole',
   PRIMARY KEY (`id_estado`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `estado_juegos`
@@ -76,7 +76,7 @@ INSERT INTO `estado_juegos` (`id_estado`, `id_grupo`, `id_usr`, `unidad`, `id_ju
 (1, 'moravia122', 'felipe@correo.de', 1, 'c01ropero1', 'Ropero', 'true', 1, 'true'),
 (2, 'moravia122', 'felipe@correo.de', 1, 'r01emociones', 'Reto', 'true', 0, 'false'),
 (3, 'moravia122', 'felipe@correo.de', 1, 'j01granja', 'Granja', 'true', 5, 'false'),
-(4, 'moravia122', 'felipe@correo.de', 1, 'c02ropero2', 'Ropero 2', 'true', 1, 'false'),
+(4, 'moravia122', 'felipe@correo.de', 1, 'c02ropero2', 'Ropero 2', 'true', 1, 'true'),
 (5, 'moravia122', 'felipe@correo.de', 2, 'j0202top10', 'Top 10', 'false', 0, 'false'),
 (6, 'moravia122', 'felipe@correo.de', 2, 'r0202seleccion', 'Selección', 'false', 0, 'false'),
 (7, 'moravia122', 'felipe@correo.de', 2, 'r0203bienaventuranzas', 'Bienaventuranzas', 'false', 0, 'false'),
@@ -84,9 +84,10 @@ INSERT INTO `estado_juegos` (`id_estado`, `id_grupo`, `id_usr`, `unidad`, `id_ju
 (9, 'moravia122', 'felipe@correo.de', 3, 'j0305acompanajesus', 'Acompaña a Jesús', 'false', 0, 'false'),
 (10, 'dddd', 'felipe@correo.de', 0, 'avatar01', 'avatar', 'true', 1, 'true'),
 (11, '8888', 'oscar.wong@correo.de', 0, 'avatar01', 'avatar', 'true', 1, 'false'),
-(12, '8888', 'oscar.wong@correo.de', 1, 'c01ropero1', 'Ropero', 'true', 1, 'true'),
-(13, '8888', 'oscar.wong@@correo.de', 1, 'j01granja', 'Granja', 'true', 5, 'false'),
-(14, '8888', 'oscar.wong@@correo.de', 1, 'c02ropero2', 'Ropero 2', 'true', 1, 'false');
+(12, '8888', 'oscar.wong@correo.de', 1, 'c01ropero1', 'Ropero', 'true', 1, 'false'),
+(13, '8888', 'oscar.wong@correo.de', 1, 'j01granja', 'Granja', 'true', 1, 'false'),
+(14, '8888', 'oscar.wong@correo.de', 1, 'c02ropero2', 'Ropero 2', 'true', 1, 'false'),
+(15, 'moravia122', 'oscar.wong@correo.de', 1, 'r01emociones', 'Reto', 'true', 0, 'false');
 
 -- --------------------------------------------------------
 
@@ -101,14 +102,15 @@ CREATE TABLE IF NOT EXISTS `objetos` (
   `etiqueta` varchar(32) NOT NULL,
   `id_usr` varchar(256) NOT NULL,
   PRIMARY KEY (`id_objetos`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `objetos`
 --
 
 INSERT INTO `objetos` (`id_objetos`, `visible`, `etiqueta`, `id_usr`) VALUES
-(1, 'false', 'sandalias', 'felipe@correo.de');
+(1, 'false', 'sandalias', 'felipe@correo.de'),
+(3, 'true', 'sandalias', 'oscar.wong@correo.de');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
