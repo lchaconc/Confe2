60def8fad6ca1a8d8970bf4a6cada1dd338f7291
lchaconc <luis.chacon.campos@gmail.com>
2019-08-31 22:32:46 -0600

    Update resetEstadoJuegos.sql

--- Scripts-SQL/resetEstadoJuegos.sql
+++ Scripts-SQL/resetEstadoJuegos.sql
@@ -4,3 +4,4 @@ insert into juego.estadoDeLosJuegos (idJuego, idUsuario, avance, terminado) valu
 insert into juego.estadoDeLosJuegos (idJuego, idUsuario, avance, terminado) values ('C01ropero1', '1026', 1, 0 )
 insert into juego.estadoDeLosJuegos (idJuego, idUsuario, avance, terminado) values ('C02ropero2', '1026', 1, 0 )
 insert into juego.estadoDeLosJuegos (idJuego, idUsuario, avance, terminado) values ('J01granja', '1026', 1, 0 )
+insert into juego.estadoDeLosJuegos (idJuego, idUsuario, avance, terminado) values ('R01emociones', '1026', 1, 0 )
