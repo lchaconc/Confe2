6f580e6767148d17bbc618aafb1529589cd1116e
lchaconc <luis.chacon.campos@gmail.com>
2019-09-01 22:29:34 -0600

    alamcena en session storage los parametros  obtenidos

--- Build/js/preload.js
+++ Build/js/preload.js
@@ -39,30 +39,36 @@ function setup () {
 	const idUsuario = url.searchParams.get("idUsuario");
 	const tipoAvatar = url.searchParams.get("tipoAvatar");
 	const tipoTraje = url.searchParams.get("tipoTraje");
-	const apiJuegos = url.searchParams.get("apiJuegos");	
-	
-/*   
-	const sexo = sessionStorage.getItem("sexo");
-    const idUsuario = sessionStorage.getItem("idUsuario");
-    const tipoAvatar = sessionStorage.getItem("tipoAvatar");
-    const tipoTraje = sessionStorage.getItem("tipoTraje");
-	const apiJuegos = sessionStorage.getItem("apiJuegos");
-*/
+	const apiJuegos = url.searchParams.get("apiJuegos");
+
+	//Logs de los parametros
+	console.log("idUsuario", idUsuario );
+    console.log("Sexo", sexo);
+    console.log("tipoAvatar", tipoAvatar );
+    console.log("tipoTraje", tipoTraje);  
+	console.log("urlGetEstadoJuegos", urlGetEstadoJuegos + idUsuario );  
+	console.log("urlGetObjetos", urlGetObjetos + idUsuario); 
 
 
-	//Forma cadena para los dos servicios con los que se coencta preload:
 	
+	
+	//Guarda en variables de session los parametros obtenidos para que Construct2 pueda utilizarlos:
+	sessionStorage.setItem("sexo", sexo);
+	sessionStorage.setItem("idUsuario", idUsuario);
+	sessionStorage.setItem("tipoAvatar", tipoAvatar);
+	sessionStorage.setItem("tipoTraje", tipoTraje);
+	sessionStorage.setItem("apiJuegos", apiJuegos);
+	
+	
+
+
+	//Forma cadena para los dos servicios con los que se coencta preload:	
 	urlGetEstadoJuegos = apiJuegos + "listaEstadoJuegos/?idUsuario=";
 	urlGetObjetos = apiJuegos + "ObtieneObjetos/?idUsuario=";
 	
 	
     
-    console.log("idUsuario", idUsuario );
-    console.log("Sexo", sexo);
-    console.log("tipoAvatar", tipoAvatar );
-    console.log("tipoTraje", tipoTraje);  
-	console.log("urlGetEstadoJuegos", urlGetEstadoJuegos + idUsuario );  
-	console.log("urlGetObjetos", urlGetObjetos + idUsuario);  	
+ 	
 
     getJson(urlGetEstadoJuegos + idUsuario, function (data) {
 		console.log("estado juegos")
