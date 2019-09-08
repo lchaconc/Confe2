7ae68c5da6c30d3ac2c4fca3302033b6d984dcd0
lchaconc <luis.chacon.campos@gmail.com>
2019-09-07 18:38:57 -0600

    revertir  parametros por url
    se obtine valores de session

--- Build/js/preload.js
+++ Build/js/preload.js
@@ -29,40 +29,14 @@ $(document).ready(function () {
 
 
 
-function setup () {
-
-	var str = window.location.href,
-    url = new URL(str);
-    console.log(url);   
-    /* obtiene parametros de la url */
-	const sexo = url.searchParams.get("sexo");
-	const idUsuario = url.searchParams.get("idUsuario");
-	const tipoAvatar = url.searchParams.get("tipoAvatar");
-	const tipoTraje = url.searchParams.get("tipoTraje");
-	const apiJuegos = url.searchParams.get("apiJuegos");
-
-	//Logs de los parametros
-	console.log("idUsuario", idUsuario );
-    console.log("Sexo", sexo);
-    console.log("tipoAvatar", tipoAvatar );
-    console.log("tipoTraje", tipoTraje);  
-	console.log("urlGetEstadoJuegos", urlGetEstadoJuegos + idUsuario );  
-	console.log("urlGetObjetos", urlGetObjetos + idUsuario); 
-
-
+function setup () {        
+    const sexo = sessionStorage.getItem("sexo");
+    const idUsuario = sessionStorage.getItem("idUsuario");
+    const tipoAvatar = sessionStorage.getItem("tipoAvatar");
+    const tipoTraje = sessionStorage.getItem("tipoTraje");
+	const apiJuegos = sessionStorage.getItem("apiJuegos");
 	
-	
-	//Guarda en variables de session los parametros obtenidos para que Construct2 pueda utilizarlos:
-	sessionStorage.setItem("sexo", sexo);
-	sessionStorage.setItem("idUsuario", idUsuario);
-	sessionStorage.setItem("tipoAvatar", tipoAvatar);
-	sessionStorage.setItem("tipoTraje", tipoTraje);
-	sessionStorage.setItem("apiJuegos", apiJuegos);
-	
-	
-
-
-	//Forma cadena para los dos servicios con los que se coencta preload:	
+	//Forma cadena para los dos servicios con los que se coencta preload:
 	urlGetEstadoJuegos = apiJuegos + "listaEstadoJuegos/?idUsuario=";
 	urlGetObjetos = apiJuegos + "ObtieneObjetos/?idUsuario=";
 	
