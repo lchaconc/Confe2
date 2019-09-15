9e1ccc2c4e6b3694a5766b33a44342bd43b3ddf0
lchaconc <luis.chacon.campos@gmail.com>
2019-09-15 14:54:34 -0600

    flappy bird

---
+++ Build/modulos/u2/flappy/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1568580711,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/btnplay-sheet0.png",
+		"images/day.png",
+		"images/suelo.png",
+		"images/bgspalsh-sheet0.png",
+		"images/bgtap-sheet0.png",
+		"images/player-sheet0.png",
+		"images/tubo2-sheet0.png",
+		"images/tubo1-sheet0.png",
+		"images/pajaromuerto-sheet0.png",
+		"images/txttitulo-sheet0.png",
+		"images/snsground-sheet0.png",
+		"images/snsground2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
