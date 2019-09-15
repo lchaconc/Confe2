687c1a1eacc7f49dc315be2c8a1935f4f6fe0e17
lchaconc <luis.chacon.campos@gmail.com>
2019-09-15 17:15:25 -0600

    build

---
+++ Build/modulos/u2/mandela/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1568581809,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bgmain-sheet0.png",
+		"images/bgspalsh-sheet0.png",
+		"images/bginstrucciones-sheet0.png",
+		"images/bgfin-sheet0.png",
+		"images/foto1-sheet0.png",
+		"images/foto2-sheet0.png",
+		"images/marca-sheet0.png",
+		"images/marca-sheet1.png",
+		"images/snssuelo-sheet0.png",
+		"images/snsmonticulo-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
---
+++ Build/modulos/u2/top10/offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1568583951,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/icoteatro-sheet0.png",
+		"images/icodinero-sheet0.png",
+		"images/icofamilia-sheet0.png",
+		"images/icoropa-sheet0.png",
+		"images/icojesus-sheet0.png",
+		"images/icocelular-sheet0.png",
+		"images/icomusica-sheet0.png",
+		"images/iconovios-sheet0.png",
+		"images/icoredes-sheet0.png",
+		"images/icoamigos-sheet0.png",
+		"images/icogym-sheet0.png",
+		"images/icobaile-sheet0.png",
+		"images/btnsiguiente-sheet0.png",
+		"images/bgmain-sheet0.png",
+		"images/bgsplash-sheet0.png",
+		"images/bgintro-sheet0.png",
+		"images/txtresultado-sheet0.png",
+		"images/sns01-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
