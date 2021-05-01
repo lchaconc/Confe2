42fd87f499a72bcb29e219ae816aa3becb6b3775
lchaconc <luis.chacon.campos@gmail.com>
2021-05-01 12:50:10 -0600

    creacion react app qqconfirmado
    y json principiante

---
+++ _DEV/U5/app-qqsconfirmado/src/App.js
@@ -0,0 +1,15 @@
+import nPrincipiante from "./data/nivel-principiante-1.json";
+
+console.log(nPrincipiante);
+
+
+function App() {
+  return (
+    <div className="container">
+      <h1>test        
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ _DEV/U5/app-qqsconfirmado/src/componentes/Splash.jsx
@@ -0,0 +1,9 @@
+export default function Splash(params) {
+
+    <div className="row">
+        <div className="col-12">
+            <h1>SPLASH</h1>
+        </div>
+    </div>
+    
+}
