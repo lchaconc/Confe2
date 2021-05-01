8efc136c1b959c56fb9cc41ddf7c0dd9527bb52a
lchaconc <luis.chacon.campos@gmail.com>
2021-05-01 13:43:16 -0600

    carga de json todos lo niveles

--- _DEV/U5/app-qqsconfirmado/src/App.js
+++ _DEV/U5/app-qqsconfirmado/src/App.js
@@ -1,6 +1,10 @@
-import nPrincipiante from "./data/nivel-principiante-1.json";
+import nPrincipiante from "./data/nivel-1-principiante.json";
+import nFacil from "./data/nivel-2-facil.json";
+import nMedio from "./data/nivel-3-medio.json";
+import nDificil from "./data/nivel-4-dificil.json";
 
-console.log(nPrincipiante);
+console.log(nDificil);
+
 
 
 function App() {
