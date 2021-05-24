1c0ad09199dd658040cd310380ef9e60a4587000
lchaconc <luis.chacon.campos@gmail.com>
2021-05-23 22:27:14 -0600

    Imp loader en voto

--- _DEV/U5/app-qqsconfirmado/src/componentes/GraficoPublico.jsx
+++ _DEV/U5/app-qqsconfirmado/src/componentes/GraficoPublico.jsx
@@ -1,18 +1,36 @@
-function getRandomInt(min, max) {
+import React, { useState, useEffect } from "react";
+
+const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 export default function GraficoPublico(props) {
   const rndNum = getRandomInt(1, 4);
   console.log(rndNum);
 
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    setup();
+  }, []);
+
+  const setup = () => {
+    setTimeout(() => {
+      setIsReady(true);
+    }, 1500);
+  };
+
   return (
     <div className="row">
       <div className="col-sm-12 text-center">
         <img
-          src={`./assets/grafico${rndNum}.png`}
+          src={
+            !isReady
+              ? "./assets/loading-vote.jpg"
+              : `./assets/grafico${rndNum}.png`
+          }
           alt="grafico"
           className="img-graphic"
         />
